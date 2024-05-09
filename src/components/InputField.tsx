991e1bec214fdda57b8fc8efae4aa3d9e0907d60
Mihai Smarandache <mihai.sanfran@gmail.com>
2024-05-08 19:19:20 -0700

    Update component

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -6,7 +6,7 @@ const InputField = () => {
     <form className="input">
       <input type="input" placeholder="Enter a task" className="input__box" />
       <button className="input__submit" type="submit">
-        Go
+        Submit
       </button>
     </form>
   );
