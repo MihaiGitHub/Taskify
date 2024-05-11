a4680bbac1d2764c669cef227333822762787cc5
Mihai Smarandache <mihai.sanfran@gmail.com>
2024-05-11 16:37:57 -0700

    Create interface for todo InputField component

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,10 +1,23 @@
 import React from "react";
 import "./styles.css";
 
-const InputField = () => {
+// setTodo type gotten from hovering over it in App.tsx
+interface Props {
+  todo: string | number;
+  setTodo: React.Dispatch<React.SetStateAction<string | number>>;
+}
+
+// define InputField type and arguments
+const InputField: React.FC<Props> = ({ todo, setTodo }) => {
   return (
     <form className="input">
-      <input type="input" placeholder="Enter a task" className="input__box" />
+      <input
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+        type="input"
+        placeholder="Enter a task"
+        className="input__box"
+      />
       <button className="input__submit" type="submit">
         Submit
       </button>
