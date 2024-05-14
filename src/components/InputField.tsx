a5459cab64d9e83f3b7f08f523486ede6f9c792e
Mihai Smarandache <mihai.sanfran@gmail.com>
2024-05-13 20:55:05 -0700

    Create handleAdd function

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -5,12 +5,13 @@ import "./styles.css";
 interface Props {
   todo: string | number;
   setTodo: React.Dispatch<React.SetStateAction<string | number>>;
+  handleAdd: (e: React.FormEvent) => void;
 }
 
 // define InputField type and arguments
-const InputField: React.FC<Props> = ({ todo, setTodo }) => {
+const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
   return (
-    <form className="input">
+    <form className="input" onSubmit={handleAdd}>
       <input
         value={todo}
         onChange={(e) => setTodo(e.target.value)}
