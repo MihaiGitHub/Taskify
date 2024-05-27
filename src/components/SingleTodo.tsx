cc17a5f3ecf02ab3f0439e4ba800064d9ed3efb5
Mihai Smarandache <mihai.sanfran@gmail.com>
2024-05-27 11:39:34 -0700

    Update type and react icons

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,7 +1,38 @@
 import React from "react";
+import { Todo } from "../model";
+import { AiFillEdit, AiFillDelete } from "react-icons/ai";
+import { MdDone } from "react-icons/md";
+import "./styles.css";
 
-const SingleTodo = () => {
-  return <div>Single Todo</div>;
+// type uses =, interface doesn't
+// Types in TypeScript are more flexible and can define primitive, intersection, union, tuple, or different types of data, while interfaces are used to describe the shape of an object.
+// One major difference between type aliases vs interfaces
+// are that interfaces are open and type aliases are closed.
+// This means you can extend an interface by declaring it a second time.
+// type cannot be changed outside of its declaration.
+type Props = {
+  todo: Todo;
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+};
+
+const SingleTodo: React.FC<Props> = ({ todo, todos, setTodos }) => {
+  return (
+    <form className="todos__single">
+      <span className="todos__single--text">{todo.todo}</span>
+      <div>
+        <span className="icon">
+          <AiFillEdit />
+        </span>
+        <span className="icon">
+          <AiFillDelete />
+        </span>
+        <span className="icon">
+          <MdDone />
+        </span>
+      </div>
+    </form>
+  );
 };
 
 export default SingleTodo;
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./styles.css";
 import { Todo } from "../model";
+import SingleTodo from "./SingleTodo";
 
 interface Props {
   todos: Todo[];
@@ -8,7 +9,18 @@ interface Props {
 }
 
 const TodoList: React.FC<Props> = ({ todos, setTodos }) => {
-  return <div className="todos"></div>;
+  return (
+    <div className="todos">
+      {todos.map((todo) => (
+        <SingleTodo
+          todo={todo}
+          key={todo.id}
+          todos={todos}
+          setTodos={setTodos}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default TodoList;
