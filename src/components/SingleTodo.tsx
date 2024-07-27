b5c1283679e62a09d8f23554337fcd04f0857764
Mihai Smarandache <mihai.sanfran@gmail.com>
2024-07-27 11:42:28 -0700

    Implement drag and drop of items feature

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -3,6 +3,7 @@ import { Todo } from "../model";
 import { AiFillEdit, AiFillDelete } from "react-icons/ai";
 import { MdDone } from "react-icons/md";
 import "./styles.css";
+import { Draggable } from "react-beautiful-dnd";
 
 // type uses =, interface doesn't
 // Types in TypeScript are more flexible and can define primitive, intersection, union, tuple, or different types of data, while interfaces are used to describe the shape of an object.
@@ -11,12 +12,13 @@ import "./styles.css";
 // This means you can extend an interface by declaring it a second time.
 // type cannot be changed outside of its declaration.
 type Props = {
+  index: number;
   todo: Todo;
   todos: Todo[];
   setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
 };
 
-const SingleTodo: React.FC<Props> = ({ todo, todos, setTodos }) => {
+const SingleTodo: React.FC<Props> = ({ index, todo, todos, setTodos }) => {
   const [edit, setEdit] = useState<boolean>(false);
   const [editTodo, setEditTodo] = useState<string>(todo.todo);
 
@@ -51,39 +53,49 @@ const SingleTodo: React.FC<Props> = ({ todo, todos, setTodos }) => {
   const inputRef = useRef<HTMLInputElement>(null);
 
   return (
-    <form className="todos__single" onSubmit={(e) => handleEdit(e, todo.id)}>
-      {edit ? (
-        <input
-          ref={inputRef}
-          value={editTodo}
-          onChange={(e) => setEditTodo(e.target.value)}
-          className="todos__single--text"
-        />
-      ) : todo.isDone ? (
-        <s className="todos__single--text">{todo.todo}</s>
-      ) : (
-        <span className="todos__single--text">{todo.todo}</span>
-      )}
-
-      <div>
-        <span
-          className="icon"
-          onClick={() => {
-            if (!edit && todo.isDone) {
-              setEdit(!edit);
-            }
-          }}
+    <Draggable draggableId={todo.id.toString()} index={index}>
+      {(provided) => (
+        <form
+          className="todos__single"
+          onSubmit={(e) => handleEdit(e, todo.id)}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          ref={provided.innerRef}
         >
-          <AiFillEdit onClick={(e) => setEdit(true)} />
-        </span>
-        <span className="icon">
-          <AiFillDelete onClick={() => handleDelete(todo.id)} />
-        </span>
-        <span className="icon">
-          <MdDone onClick={() => handleDone(todo.id)} />
-        </span>
-      </div>
-    </form>
+          {edit ? (
+            <input
+              ref={inputRef}
+              value={editTodo}
+              onChange={(e) => setEditTodo(e.target.value)}
+              className="todos__single--text"
+            />
+          ) : todo.isDone ? (
+            <s className="todos__single--text">{todo.todo}</s>
+          ) : (
+            <span className="todos__single--text">{todo.todo}</span>
+          )}
+
+          <div>
+            <span
+              className="icon"
+              onClick={() => {
+                if (!edit && todo.isDone) {
+                  setEdit(!edit);
+                }
+              }}
+            >
+              <AiFillEdit onClick={(e) => setEdit(true)} />
+            </span>
+            <span className="icon">
+              <AiFillDelete onClick={() => handleDelete(todo.id)} />
+            </span>
+            <span className="icon">
+              <MdDone onClick={() => handleDone(todo.id)} />
+            </span>
+          </div>
+        </form>
+      )}
+    </Draggable>
   );
 };
 
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./styles.css";
 import { Todo } from "../model";
 import SingleTodo from "./SingleTodo";
+import { Droppable } from "react-beautiful-dnd";
 
 interface Props {
   todos: Todo[];
@@ -10,48 +11,56 @@ interface Props {
   setCompletedTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
 }
 
-const TodoList: React.FC<Props> = ({ todos, setTodos }) => {
+const TodoList: React.FC<Props> = ({
+  todos,
+  setTodos,
+  completedTodos,
+  setCompletedTodos,
+}) => {
   return (
     <div className="container">
-      <div className="todos">
-        <span className="todos__heading">Active Tasks</span>
-        {todos.map((todo) => (
-          <SingleTodo
-            todo={todo}
-            todos={todos}
-            key={todo.id}
-            setTodos={setTodos}
-          />
-        ))}
-      </div>
-      <div className="todos remove">
-        <span className="todos__heading">Completed Tasks</span>
-        {todos.map((todo) => (
-          <SingleTodo
-            todo={todo}
-            todos={todos}
-            key={todo.id}
-            setTodos={setTodos}
-          />
-        ))}
-      </div>
+      <Droppable droppableId="TodosList">
+        {(provided) => (
+          <div
+            className="todos"
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+          >
+            <span className="todos__heading">Active Tasks</span>
+            {todos.map((todo, index) => (
+              <SingleTodo
+                index={index}
+                todo={todo}
+                todos={todos}
+                key={todo.id}
+                setTodos={setTodos}
+              />
+            ))}
+          </div>
+        )}
+      </Droppable>
+      <Droppable droppableId="TodosRemove">
+        {(provided) => (
+          <div
+            className="todos remove"
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+          >
+            <span className="todos__heading">Completed Tasks</span>
+            {completedTodos.map((todo, index) => (
+              <SingleTodo
+                index={index}
+                todo={todo}
+                todos={completedTodos}
+                key={todo.id}
+                setTodos={setCompletedTodos}
+              />
+            ))}
+          </div>
+        )}
+      </Droppable>
     </div>
   );
 };
-
-// const TodoList: React.FC<Props> = ({ todos, setTodos }) => {
-//   return (
-//     <div className="todos">
-//       {todos.map((todo) => (
-//         <SingleTodo
-//           todo={todo}
-//           key={todo.id}
-//           todos={todos}
-//           setTodos={setTodos}
-//         />
-//       ))}
-//     </div>
-//   );
-// };
 
 export default TodoList;
